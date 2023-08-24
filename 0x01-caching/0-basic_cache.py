9495116364c85468430c5f25e647974434550a79
Crysart <idukrystal@gmail.com>
2023-08-24 01:23:15 +0100

    00 t1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+Creates a class BasicCache that
+inherits from BaseCaching and is a caching system:
+and is a caching system:
+'''
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' A Basic Cacheing Class  '''
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        return self.cache_data[key]
